f329099a25d8c8094a505e55fa5587313ddac696
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-08-20 11:26:00 -0430

    videos pagina webgob

--- system/webgob/web/views/galeria/movie.php
+++ system/webgob/web/views/galeria/movie.php
@@ -1,5 +1,37 @@
-<div class="title_section cufon_myriad">Programa 133 - Rindiendo Cuentas</div>
-<video poster="<?php print URL; ?>site/template/img/tapa_video.jpg" controls="controls" preload="none" width="564" class="video">
+<div class="title_section cufon_myriad">Programa 155 - Rindiendo Cuentas</div>
+<object width="425" height="355">
+  <param name="movie"
+         value="http://www.youtube.com/v/ujcioaxEmls?version=3
+&autohide=1&showinfo=0"></param>
+  <param name="allowScriptAccess" value="always"></param>
+  <embed src="http://www.youtube.com/v/ujcioaxEmls?version=3
+&autohide=1&showinfo=0"
+         type="application/x-shockwave-flash" allowscriptaccess="always"
+         width="425" height="355"></embed>
+</object>
+<div class="title_section cufon_myriad">Programa 154 - Rindiendo Cuentas</div>
+<object width="425" height="355">
+  <param name="movie"
+         value="http://www.youtube.com/v/Vr7qid4qwok?version=3
+&autohide=1&showinfo=0"></param>
+  <param name="allowScriptAccess" value="always"></param>
+  <embed src="http://www.youtube.com/v/Vr7qid4qwok?version=3
+&autohide=1&showinfo=0"
+         type="application/x-shockwave-flash" allowscriptaccess="always"
+         width="425" height="355"></embed>
+</object>
+<div class="title_section cufon_myriad">Programa 153 - Rindiendo Cuentas</div>
+<object width="425" height="355">
+  <param name="movie"
+         value="http://www.youtube.com/v/TexKIuJm2YY?version=3
+&autohide=1&showinfo=0"></param>
+  <param name="allowScriptAccess" value="always"></param>
+  <embed src="http://www.youtube.com/v/TexKIuJm2YY?version=3
+&autohide=1&showinfo=0"
+         type="application/x-shockwave-flash" allowscriptaccess="always"
+         width="425" height="355"></embed>
+</object>
+<!--<video poster="<?php print URL; ?>site/template/img/tapa_video.jpg" controls="controls" preload="none" width="564" class="video">
     <source src="<?php print URL; ?>media/movie/programa/rc/6/6.theora.ogv" />
     <source src="<?php print URL; ?>media/movie/programa/rc/6/6.mp4video.mp4" />
     <source src="<?php print URL; ?>media/movie/programa/rc/6/6.webmvp8.webm" />
@@ -42,4 +74,4 @@ Descargar Programa
     <li><a href="<?php print URL; ?>media/movie/programa/rc/3/3.theora.ogv">Theora .ogv</a></li>
     <li><a href="<?php print URL; ?>media/movie/programa/rc/3/3.mp4video.mp4">MPEG-4 .mp4</a></li>
     <li><a href="<?php print URL; ?>media/movie/programa/rc/3/3.webmvp8.webm">WebM .webm</a></li>
-</ul>
+</ul>-->
