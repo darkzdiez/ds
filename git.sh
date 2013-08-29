d476ff6bc0cdea709b171b242068891fcfc174c2
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-08-29 09:12:40 -0430

    git.sh

--- git.sh
+++ git.sh
@@ -1,19 +1,28 @@
 #/bin/bash
 # var project
 read -p 'Ingrese Nombre del Projecto: ' project
-read -p 'Subir al Servidor FTP[0:NO, 1:SI]: ' ftp
+read -p 'Subir al Servidor FTP [Y,n]: ' ftp
 read -p 'Mensaje: ' mensaje
 echo 'Trabajando...'
 echo 'Agregando Archivos Locales'
+sleep 2
 git add --all
 echo 'commit Archivos Locales'
+sleep 2
 git commit -a -m $mensaje
-
+echo 'Verificando cambios Remotos'
+sleep 2
 git pull git@github.com:darkzdiez/$project.git
-
+echo 'Enviando cambios'
+sleep 2
 git push git@github.com:darkzdiez/$project.git
 # define ftp remote folder
-if [ ftp=='1' ]; then
+if [[ $ftp == "Y" || $ftp == "y" ]]; then
+	echo "Subiendo FTP"
+	sleep 2
 	git ftp push --user diezsolu --passwd 027pcN9Hnw ftp://diezsoluciones.com/public_html
+else
+        echo "No se Subira al FTP"
+        sleep 2
 fi
 echo 'Finalizado'
