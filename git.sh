39d760a5d2dd269196736c66380341dd1de9d425
darkzdiez <you@example.com>
2013-08-29 07:21:24 -0430

    git.sh

---
+++ git.sh
@@ -0,0 +1,9 @@
+git add --all
+
+git commit -a -m 'git.sh'
+
+git pull git@github.com:darkzdiez/webt.git
+
+git push git@github.com:darkzdiez/webt.git
+
+git ftp push --user diezsolu --passwd 027pcN9Hnw ftp://diezsoluciones.com/public_html
