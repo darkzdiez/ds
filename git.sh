16840dd7d2b45c4e1e84cabc343ddd823d311aad
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-08-29 09:03:22 -0430

    git.sh

--- git.sh
+++ git.sh
@@ -1,16 +1,17 @@
 #/bin/bash
 # var project
-echo 'Ingrese Nombre del Projecto'
-read project;
-
+read -p 'Ingrese Nombre del Projecto: ' project
+read -p 'Subir al Servidor FTP[0:NO, 1:SI]: ' ftp
 echo 'Trabajando...'
 git add --all
 
 git commit -a -m 'git.sh'
 
-git pull git@github.com:darkzdiez/$(project).git
+git pull git@github.com:darkzdiez/$project.git
 
-git push git@github.com:darkzdiez/$(project).git
+git push git@github.com:darkzdiez/$project.git
 # define ftp remote folder
-git ftp push --user diezsolu --passwd 027pcN9Hnw ftp://diezsoluciones.com/public_html
+if [[ ftp=1 ]]; then
+	git ftp push --user diezsolu --passwd 027pcN9Hnw ftp://diezsoluciones.com/public_html
+fi
 echo 'Finalizado'
