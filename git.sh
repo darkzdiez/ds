1489cbae97688e59667d0838b48c0835b5e7f20f
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-08-29 08:50:43 -0430

    git.sh

--- git.sh
+++ git.sh
@@ -1,9 +1,16 @@
+#/bin/bash
+# var project
+echo 'Ingrese Nombre del Projecto'
+read project;
+
+echo 'Trabajando...'
 git add --all
 
 git commit -a -m 'git.sh'
 
-git pull git@github.com:darkzdiez/webt.git
+git pull git@github.com:darkzdiez/$(project).git
 
-git push git@github.com:darkzdiez/webt.git
-
+git push git@github.com:darkzdiez/$(project).git
+# define ftp remote folder
 git ftp push --user diezsolu --passwd 027pcN9Hnw ftp://diezsoluciones.com/public_html
+echo 'Finalizado'
