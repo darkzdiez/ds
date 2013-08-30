27855f2d0127544290a52cc6df4a21f3c9c7c8c2
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-08-30 10:47:02 -0430

    general config

--- generalConfig.php
+++ generalConfig.php
@@ -6,8 +6,8 @@ define('_pathSYSTEM', 'system/');
 define('PROJECTNAME', 'webt');
 define('SYSTEMDEFAULT','webgob');
 $listSYSTEM = array(
-    'pgchost.com' => 'webgob',
-    'www.pgchost.com' => 'webgob',
+    'yaracuy.gob.ve' => 'ordenes',
+    'www.yaracuy.gob.ve' => 'ordenes',
     'primasalud.com.ve' => 'prims',
     'www.primasalud.com.ve' => 'prims'
 );
