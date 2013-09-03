7eb8cbeb7ac880181daac94285b0a9553f4f4398
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-09-03 10:48:59 -0430

    detalles de prims

--- system/prims/principal/views/template/header.php
+++ system/prims/principal/views/template/header.php
@@ -4,9 +4,9 @@
   <meta charset="utf-8">
   <title>Titulo P</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <script type="text/javascript">
+  <meta name="description" content="IAMEG - Instituto Autonomo de Minerales del Estado Guarico">
+  <meta name="author" content="DiezSoluciones.com">
+  <meta name="google-site-verification" content="y9bAKLKaxMObeSSjefRuyogE7Uzw5ylw_q_jBhMism8" />  <script type="text/javascript">
   DOMAIN = '<?php print DOMAIN; ?>';
   PATH_SYSTEM = '<?php print PATH_SYSTEM; ?>';
   PATH_FILE = '<?php print PATH_FILE; ?>';
@@ -24,7 +24,7 @@
   ?>
   <link href="<?php print DOMAIN; ?>public/css/font-awesome.min.css" rel="stylesheet">
   <link href="<?php print DOMAIN; ?>public/css/font-awesome-ie7.min.css" rel="stylesheet">
-  <link rel="shortcut icon" href="<?php echo DOMAIN; ?>public/ico/favicon.png">
+  <link rel="shortcut icon" href="<?php print PATH_FILE; ?>public/images/favicon.ico">
   <script type="text/javascript" src="<?php print DOMAIN; ?>public/js/jquery.js"></script>
   <script type="text/javascript" src="<?php print DOMAIN; ?>public/js/less.js"></script>
   <script type="text/javascript" src="<?php print DOMAIN; ?>public/js/general.js"></script>
