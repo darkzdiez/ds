9909372b723236a5ed4552fbb16b9d30ae27855c
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-09-05 14:24:09 -0430

    test

--- system/webgob/config.php
+++ system/webgob/config.php
@@ -3,9 +3,4 @@
 $DEFAULT_MODULE = 'web';
 $MODULE = array('admin');
 define('DIR_PATH_SYSTEM', dirname(__FILE__));
-if (!$_SERVER['REMOTE_ADDR'] == '::1' OR !$_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
-	ini_set('track_errors', 0);
-	ini_set('error_reporting', 0);
-	ini_set('display_errors', false);
-}
 ?>
