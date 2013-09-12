cfef8cc556e977f05aed64ad812d3e2a5b99353c
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-09-12 16:15:17 -0430

    Revert "commit 2013-09-12 03:19:28 pm"
    This reverts commit db0f7a96695887028a4cde495ed28fec092acd3d.

--- system/webgob/admin/views/perfil/js/default.js
+++ system/webgob/admin/views/perfil/js/default.js
@@ -1,14 +1,10 @@
 function guardarEditarPerfil(data){
 	ajaxLoaderClose();
 	alert(data.mensaje);
-	document.getElementById("cambiarClave").reset();
-}
-
-function checkpass(event){
-	if ($('#clave').val()==$('#reclave').val()) {
-		return true;
-	} else{
-		alert('Las claves no coinciden');
-	    return false;
+	if (data.seguridadPass=='b') {
+		$().addClass('in');
+		alertBS('open', "#alertClaveBS");
+	}else{
+		alertBS('close', "#alertClaveBS");
 	};
 }
