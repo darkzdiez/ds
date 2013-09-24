02d06fda840a1f52752b772f3722361abc3b1ca3
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-09-24 17:14:12 -0430

    commit 2013-09-24 05:14:08 pm

--- libs/Database.php
+++ libs/Database.php
@@ -8,7 +8,8 @@ class Database extends PDO
     {
         parent::__construct(
             $DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASS,
-            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8;
+                SET GLOBAL general_log = 'ON'"));
         
         //parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTIONS);
     }
