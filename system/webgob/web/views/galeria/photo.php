0f9f71c00f26b8d0c914cffb596ae303508ee88c
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-10-14 17:03:47 -0430

    commit 2013-10-14 05:03:42 pm

--- system/webgob/web/views/galeria/photo.php
+++ system/webgob/web/views/galeria/photo.php
@@ -7,7 +7,7 @@ foreach ($results['result'] as $key => $result) {
 		$photo = array_values(array_diff($photo, array('thumbnail','..','.')));
 	    //$imgPpal = $this->model->showPpalImg($result['idgallery']);
 		?>
-		<a href="<?php print PATH_NAV . 'galeria/seephoto/' . $result['idgallery']; ?>" class="photoGallery" style="background-image: url('<?php print PATH_SYSTEM . 'media/images/galeria/' . $result['idgallery'] . '/' . $photo[0]; ?>'">
+		<a href="<?php print PATH_NAV . 'galeria/seephoto/' . $result['idgallery']; ?>" class="photoGallery" style="background-image: url('<?php print PATH_SYSTEM . 'media/images/galeria/' . $result['idgallery'] . '/' . $photo[0]; ?>')">
 			<div class="infoPanel"><?php print strtoupper($result['name']); ?></div>
 		</a>
 		<?php
