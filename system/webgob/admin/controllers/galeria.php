8a6d04b93c992ead395af891416b9ec1ce607457
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-10-14 09:55:29 -0430

    test

--- system/webgob/admin/controllers/galeria.php
+++ system/webgob/admin/controllers/galeria.php
@@ -23,7 +23,7 @@ class Galeria extends Controller {
         print json_encode($this->model->listarGaleria());
     }
     public function crear() {
-        exit($_POST['nombre']);
+        exit(stripslashes($_POST['nombre']));
         print json_encode($this->model->crear($_POST));
     }
 
