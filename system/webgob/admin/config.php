58772d571325f75a059e713bc47bf0bfe265cc49
alfonzoOficina <alfonzooficina@alfonzooficina-Compaq-500B-Microtower>
2013-10-14 17:06:07 -0430

    commit 2013-10-14 05:06:03 pm

--- system/webgob/admin/config.php
+++ system/webgob/admin/config.php
@@ -13,13 +13,13 @@ if ($_SERVER['REMOTE_ADDR'] == '::1' OR $_SERVER['REMOTE_ADDR'] == '127.0.0.1') 
     define('DB_PASS', 'root');
     define('SESSIONPATH','/w/webgob/admin/');
 } else {
-    define('PATH_NAV', 'http://yaracuy.gob.ve/' . 'web2/admin/');
+    define('PATH_NAV', 'http://yaracuy.gob.ve/' . 'web/admin/');
     define('DB_TYPE', 'mysql');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'sitioweb2');
     define('DB_USER', 'root');
     define('DB_PASS', 'dbadmin$yara$2010');
-    define('SESSIONPATH','/web2/admin/');
+    define('SESSIONPATH','/web/admin/');
 }
 define('PATH_SYSTEM', DOMAIN . 'system/webgob/');
 define('PATH_FILE', PATH_SYSTEM . 'admin/');
