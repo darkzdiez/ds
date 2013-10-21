add3fdd37393e8c5ebf01096db029b47c455ed32
Alfonzo Diez <alfonzodiez@gmail.com>
2013-10-21 10:41:36 -0430

    test

--- system/webgob/web/views/banner/b282x236.php
+++ system/webgob/web/views/banner/b282x236.php
@@ -1,12 +1,1 @@
-<!--<a href="<?php print PATH_NAV; ?>" class="spritex282 spritex282-MORRALES-pag"></a>
-<a href="<?php print PATH_NAV; ?>" class="spritex282 spritex282-PUEBLO-SANO-pag"></a>
-<a href="<?php print PATH_NAV; ?>" class="spritex282 spritex282-TANQUES"></a>
-<a href="<?php print PATH_NAV; ?>" class="spritex282 spritex282-viviendas-pag"></a>-->
-<?php
-$data = array_diff($this->imagenes, array('thumbnail','..','.'));
-foreach ($data as $key => $value) {
-    ?>
-    <a href="<?php print PATH_NAV; ?>" class="spritex282"><img src="<?php print PATH_SYSTEM . 'media/images/banner/1/' . $value; ?>"></a>
-    <?php
-}
-?>
+ads
