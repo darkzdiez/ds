942235d044afddb8547a18a887d838af75d8eca4
Alfonzo Diez <alfonzodiez@gmail.com>
2013-10-21 10:42:52 -0430

    test

--- system/webgob/web/controllers/banner.php
+++ system/webgob/web/controllers/banner.php
@@ -4,6 +4,7 @@ class Banner extends Controller {
     public function index() {
     }
     public function b282x236(){
+    	exit('hola');
 		$this->view->imagenes=scandir('system/webgob/media/images/banner/1');
 		print $this->view->getContent('banner/b282x236');
     }
