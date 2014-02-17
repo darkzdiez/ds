1bcc971f7bf61ee4fd8775238b4e2e70d5ab5b69
darkzdiez <you@example.com>
2014-02-16 23:15:24 -0430

    general

--- commit.sh
+++ commit.sh
@@ -1,27 +1,23 @@
 #/bin/bash
 
-git config --global push.default simple
 git add --all
 git commit -a --reset-author -m "commit $(date +%F' '%r)"
 git pull
 git push
 
 cd system/webgob/
-git config --global push.default simple
 git add --all
 git commit -a --reset-author -m "commit $(date +%F' '%r)"
 git pull
 git push
 
 cd ../ordenes/
-git config --global push.default simple
 git add --all
 git commit -a --reset-author -m "commit $(date +%F' '%r)"
 git pull
 git push
 
 cd ../ihavey/
-git config --global push.default simple
 git add --all
 git commit -a --reset-author -m "commit $(date +%F' '%r)"
 git pull
